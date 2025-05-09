6cb60dbb0e8cacd049ed0487492d5933525a8482
whymaull <first.wahyumaul21@gmail.com>
2025-05-09 18:32:15 +0700

    update

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 from flask import Flask
-import pymysql
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
 import os
+
+# Load .env file
+load_dotenv()
 
 app = Flask(__name__)
 
+# Ambil URL DB dari environment
+db_url = os.getenv("DB_URL")
+engine = create_engine(db_url)
+
 @app.route('/')
 def index():
-    return 'Hello from Railway + Flask + MySQL!'
+    try:
+        with engine.connect() as conn:
+            result = conn.execute("SELECT NOW();")
+            return f"Database connected! Current time: {list(result)[0][0]}"
+    except Exception as e:
+        return f"Failed to connect to DB: {str(e)}"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
