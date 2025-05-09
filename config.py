e7d1dbbedead429c911f8c6f1ed4f9747cad40e5
whymaull <first.wahyumaul21@gmail.com>
2025-05-09 19:59:23 +0700

    fix: DB config pakai os.environ fallback

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
+import os
+
 DB_CONFIG = {
-    "host": "mysql.railway.internal",
-    "user": "root",
-    "password": "SNLPJJsJpjsDEuYxTtTikbUGuIaoNcSy",
-    "database": "railway"
+    "host": os.environ.get("MYSQLHOST", "mysql.railway.internal"),
+    "user": os.environ.get("MYSQLUSER", "root"),
+    "password": os.environ.get("MYSQLPASSWORD", "SNLPJJsJpjsDEuYxTtTikbUGuIaoNcSy"),
+    "database": os.environ.get("MYSQLDATABASE", "railway"),
+    "port": int(os.environ.get("MYSQLPORT", 3306))
 }
